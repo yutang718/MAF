2526d9b9c9b02cfef457f368da1c7a662f4709cb
yutang718 <yutang718@hotmail.com>
2025-04-07 12:02:57 +0800

    update log

--- frontend/pages/islamic_rules.py
+++ frontend/pages/islamic_rules.py
@@ -11,7 +11,7 @@ from core.logging import get_logger
 import uuid
 import pandas as pd
 
-logger = get_logger(__name__)
+logger = get_logger("frontend.pages.islamic_rules")
 
 def render_intro_tab():
     """Render introduction tab"""
